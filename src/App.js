28d9130569bcd43cdd57ad38305c97f33dea3e58
barros <barrospj@bc.edu>
2019-06-16 12:52:20 -0400

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TopBar } from './components/TopBar';
-import { Profile } from './components/Profile';
-import { Footer } from './components/Footer';
+import TopBar from './components/TopBar';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
 
 function App() {
   return (
